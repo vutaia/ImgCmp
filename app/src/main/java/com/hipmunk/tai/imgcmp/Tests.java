43ab22839c5b2c163ba8096ba2d608b6648ba02e
Tai Vu <vutaia@gmail.com>
2017-01-27 11:05:37 -0800

    beautify + one more test

--- app/src/main/java/com/hipmunk/tai/imgcmp/Tests.java
+++ app/src/main/java/com/hipmunk/tai/imgcmp/Tests.java
@@ -6,17 +6,14 @@ import static com.hipmunk.tai.imgcmp.R.raw.a_base;
 import static com.hipmunk.tai.imgcmp.R.raw.a_compare;
 import static com.hipmunk.tai.imgcmp.R.raw.b_base;
 import static com.hipmunk.tai.imgcmp.R.raw.b_compare;
-
-/**
- * Created by tai on 1/24/17.
- */
+import static com.hipmunk.tai.imgcmp.R.raw.c_base;
+import static com.hipmunk.tai.imgcmp.R.raw.c_compare;
 
 public class Tests {
 
     public static ImageTest[] TESTS = {
             new ImageTest(a_base, a_compare, .90f),
+            new ImageTest(c_base, c_compare, .90f),
             new ImageTest(b_base, b_compare, .90f),
-//            new ImageTest(a_base, b_compare, .90f),
-//            new ImageTest(b_base, a_compare, .90f),
     };
 }
